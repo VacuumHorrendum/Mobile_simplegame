14bbbb7a3c88f692a773c67c74f63502b180041c
VacuumHorrendum <NastyTrent@mail.ru>
2022-04-21 11:32:16 +0500

    SimpleGame

---
+++ app/src/main/java/com/example/simplegames/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.simplegames;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onClick(View view) {
+        // Создаем объект Intent для вызова новой Activity
+        Intent intent = new Intent(this,Second.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT);
+        startActivity(intent);
+    }
+}
+
